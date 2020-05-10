6128f15a3e18fb82b495d20ca855edad462a70db
Michał Chudzik <m.chudzik@binarapps.com>
2020-05-10 12:10:38 +0200

    [Add] Swimlane charts

---
+++ src/app/core/modules/charts.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+@NgModule({
+  exports: [
+    NgxChartsModule,
+  ],
+})
+export class ChartsModule {}
