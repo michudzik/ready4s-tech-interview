73994e15c1cbb9b56ba3a70656badf8f0d300382
Michał Chudzik <m.chudzik@binarapps.com>
2020-05-10 12:41:51 +0200

    [Add] Exchange Rate Service [Update] Module structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './core/menu/menu.component';
@@ -18,6 +19,7 @@ import { MaterialModule }from './core/modules/material.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     NoopAnimationsModule,
     ChartsModule,
